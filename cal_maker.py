1cad0cf814967edfeec1b165129e7b8147cf5bb8
Che <30403707+Che-Zhu@users.noreply.github.com>
2021-01-28 16:53:18 +0800

    v1.0 code
    Python 1.0 code, ready to release

--- cal_maker.py
+++ cal_maker.py
@@ -7,11 +7,7 @@ import os
 au_timezone = pytz.timezone('Australia/Adelaide')
 
 
-def __init__(self):
-    print('cal_maker.py')
-
-
-def timezone_adelaide_to_UTC(adelaide_time):
+def timezone_adelaide_to_utc(adelaide_time):
     local = au_timezone.localize(adelaide_time)
     utc_time = local.astimezone(pytz.utc)
     return utc_time.strftime('%Y%m%dT%H%M%SZ')
@@ -19,16 +15,15 @@ def timezone_adelaide_to_UTC(adelaide_time):
 
 def converter(timetable):
     num_of_classes = len(timetable)
-    print("Your total number of classes is: " + str(num_of_classes))
 
     #  Generating .ics file
     cal = Calendar()
-    cal.add('PRODID', '-//Che-dev//timetable 1.0//EN')
+    cal.add('PRODID', '-//Che-dev//timetable//EN')
     cal.add('version', '2.0')
     cal.add('CATEGORIES', 'EDUCATION')
     # cal.add('URL', "None") TODO: to be enabled on server version
-    cal.add('NAME', 'UoA Timetable')
-    cal.add('X-WR-CALNAME', 'UoA Timetable')
+    cal.add('NAME', 'UoA Timetable v1.0-beta')
+    cal.add('X-WR-CALNAME', 'UoA Timetable v1.0-beta')
 
     for course in timetable:
         current_time = datetime.now(tz=pytz.utc).strftime('%Y%m%dT%H%M%SZ')
@@ -42,8 +37,8 @@ def converter(timetable):
         event['dtstamp'] = current_time
         event['uid'] = current_time + str(start_time) + str(random.randint(1, 1000)) + 'Che-dev@outlook.com'
 
-        event['dtstart'] = timezone_adelaide_to_UTC(start_time)
-        event['dtend'] = timezone_adelaide_to_UTC(end_time)
+        event['dtstart'] = timezone_adelaide_to_utc(start_time)
+        event['dtend'] = timezone_adelaide_to_utc(end_time)
 
         event['SUMMARY'] = course['type'] + ': ' + course['course']
         cal.add_component(event)
@@ -51,3 +46,5 @@ def converter(timetable):
     f = open(os.path.join(os.getcwd(), 'generated.ics'), 'wb')
     f.write(cal.to_ical())
     f.close()
+    print('Your timetable generated.ics has been successfully exported in directory: {} with {} classes.'
+          .format(os.getcwd(), str(num_of_classes)))
--- main.py
+++ main.py
@@ -1,84 +1,96 @@
 import requests
 import cal_maker
 
-# Necessary urls
+# Info and Urls
+program_version = 'v1.0'
 home_url = 'https://tt.openshift.services.adelaide.edu.au/login'
 callback_url = 'https://tt.openshift.services.adelaide.edu.au/login/callback'
 timetable_url = 'https://tt.openshift.services.adelaide.edu.au/api/timetable'
 
 user_name_prefix = 'uofa\\'
-user_name = input('Enter user name (axxxxxxx):')
-password = input('Enter password:')
 
-data = {'AuthMethod': 'FormsAuthentication',
-        'UserName': user_name_prefix + user_name,
-        'Password': password}
+while True:
 
-# Session start
-print('Connecting to university server, please wait...')
 
-session = requests.Session()
+    print('\r')
+    print('*'*25, program_version, '*'*25)
 
-# A response that contains SAML request and cookies
-home_response = session.get(home_url)
-print('Connected!', 'number of cookies', str(len(session.cookies.get_dict())))
+    user_name = input('Enter user name (axxxxxxx):')
+    password = input('Enter password:')
+    print('*' * 56)
+    print('\r')
 
-# Get client request id
-login_url = home_response.url
-client_id_position = home_response.text.index('client-request-id=')
-client_request_id = home_response.text[client_id_position:client_id_position + 54]
+    data = {'AuthMethod': 'FormsAuthentication',
+            'UserName': user_name_prefix + user_name,
+            'Password': password}
 
-# Login header
-login_headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive',
-    'Host': 'adfs.adelaide.edu.au',
-    'Origin': 'https://adfs.adelaide.edu.au',
-    'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
-    'Referer': login_url,
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-User': '?1',
-    'Upgrade-Insecure-Requests': '1'}
+    # Session start
+    print('Connecting to university server, please wait...')
 
-# Logging in
-print("Logging in...")
-login_full_url = login_url + '&' + client_request_id
-login_response = session.post(login_full_url, data=data, headers=login_headers, allow_redirects=False)
-print('First handshake was successful!', 'number of cookies', str(len(session.cookies.get_dict())))
+    session = requests.Session()
 
-# Get cookie
-login_response = session.get(login_full_url, headers=login_headers)
-print('Second handshake was successful!', 'number of cookies', str(len(session.cookies.get_dict())))
+    # A response that contains SAML request and cookies
+    home_response = session.get(home_url) # 2 cookies should be received
+    print('Connected!')
 
-# Get SAMLResponse
-saml_response = login_response.content.decode().partition('name="SAMLResponse" value="')
-saml_response = saml_response[2].partition('" /><noscript><p>Script is disabled. Click Submit to continue.')[0]
-saml_response = {'SAMLResponse': saml_response}
+    # Get client request id
+    login_url = home_response.url
+    client_id_position = home_response.text.index('client-request-id=')
+    client_request_id = home_response.text[client_id_position:client_id_position + 54]
 
-# Callback header
-callback_headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive',
-    'Host': 'tt.openshift.services.adelaide.edu.au',
-    'Origin': 'https://adfs.adelaide.edu.au',
-    'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
-    'Referer': 'https://adfs.adelaide.edu.au/',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'same-site',
-    'Upgrade-Insecure-Requests': '1'}
+    # Login header
+    login_headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'Host': 'adfs.adelaide.edu.au',
+        'Origin': 'https://adfs.adelaide.edu.au',
+        'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
+        'Referer': login_url,
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1'}
 
-# Callback
-callback = session.post(callback_url, data=saml_response, headers=callback_headers)
-print("Confirming SAML response with server...", str(len(session.cookies.get_dict())))
+    # Logging in
+    print("Logging in...")
+    login_full_url = login_url + '&' + client_request_id
+    login_response = session.post(login_full_url, data=data, headers=login_headers, allow_redirects=False) # 3 cookies should be received
+    if len(session.cookies.get_dict()) < 3:
+        print('Your Username or Password is incorrect, please try again')
+        continue
+
+    # Get cookie
+    login_response = session.get(login_full_url, headers=login_headers) # 6 cookies should be received
+
+    # Get SAMLResponse
+    saml_response = login_response.content.decode().partition('name="SAMLResponse" value="')
+    saml_response = saml_response[2].partition('" /><noscript><p>Script is disabled. Click Submit to continue.')[0]
+    saml_response = {'SAMLResponse': saml_response}
+
+    # Callback header
+    callback_headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'Host': 'tt.openshift.services.adelaide.edu.au',
+        'Origin': 'https://adfs.adelaide.edu.au',
+        'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
+        'Referer': 'https://adfs.adelaide.edu.au/',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-site',
+        'Upgrade-Insecure-Requests': '1'}
+
+    # Callback
+    callback = session.post(callback_url, data=saml_response, headers=callback_headers)
+    print("Confirming SAML response with server...") # 6 cookies should be received
+    break
 
 # Access api and preprocessing retrieved data
 timetable_response = session.get(timetable_url, headers=callback_headers)
